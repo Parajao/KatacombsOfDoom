dd5255567021587c66669c5b0a2dabdc701d3d6e
Alessandro Di Gioia <Parajao@Alessandros-MacBook-Pro.local>
2017-06-18 20:22:06 +0100

    Add test examples for Expecto

--- KatacombsOfDoomTests/KatacombsOfDoomTests/KatacombsOfDoomTests.fs
+++ KatacombsOfDoomTests/KatacombsOfDoomTests/KatacombsOfDoomTests.fs
@@ -1,8 +1,24 @@
 module KatacombsOfDoomTests
+
 open Expecto
 
+[<Tests>]
 let tests =
-  test "A simple test" {
-    let subject = "Hello World"
-    Expect.equal subject "Hello World" "The strings should equal"
-  }
+  testList "A test group" [
+    test "one test" {
+      Expect.equal (2+2) 4 "2+2"
+    }
+
+    test "another test that fails" {
+      let sum = 3 + 2
+      Expect.equal sum  5 "3+2"
+    }
+
+    testCase "universe exists (╭ರᴥ•́)" <| fun _ ->
+      let subject = true
+      Expect.isTrue subject "I compute, therefore I am."
+  ]
+
+[<EntryPoint>]
+let main argv =
+  Tests.runTestsInAssembly defaultConfig argv
